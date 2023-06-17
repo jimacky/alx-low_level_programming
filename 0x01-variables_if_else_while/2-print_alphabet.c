10dc9aaded0823f7561f7aff2b066a0f0a6ba533
jimacky <jimaweyo0@gmail.com>
2023-06-17 05:46:00 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - prints alphabet lowercase
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+char lc;
+for (lc == 'a'; lc <= 'z'; lc++)
+{
+putchar(lc);
+}
+putchar('\n');
+return (0);
+}
