d6e8e96da2af861f5009d9dc09f9907b36ba0beb
jimacky <jimaweyo0@gmail.com>
2023-06-20 07:04:10 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*prints_alphabet - lower case alphabet and a new line
+*Return: Always 0
+*/
+void print_alphabet(void)
+{
+char lca;
+for (lca = 'a'; lca <= 'z'; lca++)
+_putchar(la);
+_putchar('\n');
+}
