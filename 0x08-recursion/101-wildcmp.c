58a8b02d22ba7c6d3d7835f06516eac6f07ef70a
jimacky <jimaweyo0@gmail.com>
2023-07-06 13:18:16 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,19 +1,73 @@
 #include "main.h"
 #include <stdio.h>
+int strlen_no_wilds(char *str);
+void iterate_ver(char **ver);
+char *aweyo_match(char *str, char *aweyo);
+int wildcmp(char *s1, char *s2);
 /**
-*wildcmp - compares two strings
-*@s1: string 1.
-@s2: string 2. it can contains a * as a special character
-*Return: 1 if are identical, 0 if not.
+* strlen_no_wilds - Returns the length of a string,
+* @str: The string to be measured.
+* Return: The length.
+*/
+int strlen_no_wilds(char *str)
+{
+int len = 0, index = 0;
+if (*(str + index))
+{
+if (*str != '*')
+len++;
+index++;
+len += strlen_no_wilds(str + index);
+}
+return (len);
+}
+/**
+* iterate_ver - Iterates through a string located at a wildcard
+* @ver: The string to be iterated through.
+*/
+void iterate_ver(char **ver)
+{
+if (**ver == '*')
+{
+(*ver)++;
+iterate_ver(ver);
+}
+}
+/**
+* aweyo_match - Checks if a string str matches the postfix
+* @str: The string to be matched.
+* @aweyo: The name
+* Return: If str and postfix are identical - a pointer to the null byte
+*/
+char *aweyo_match(char *str, char *aweyo)
+{
+int str_len = strlen_no_wilds(str) - 1;
+int aweyo_len = strlen_no_wilds(aweyo) - 1;
+if (*aweyo == '*')
+iterate_ver(&aweyo);
+if (*(str + str_len - aweyo_len) == *aweyo && *aweyo != '\0')
+{
+aweyo++;
+return (aweyo_match(str, aweyo));
+}
+return (aweyo);
+}
+/**
+* wildcmp - Compares two strings, considering wildcard characters
+* @s1: The first string to be compared
+* @s2: The second string to be compared - may contain ver
+* Return: If the strings can be considered identical - 1
 */
 int wildcmp(char *s1, char *s2)
 {
-if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
-if (*s1 == '\0' && *s2 == '\0')
-return (1);
-if (*s1 == *s2)
-return (wildcmp(s1 + 1, s2 + 1));
 if (*s2 == '*')
-return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+{
+iterate_ver(&s2);
+s2 = aweyo_match(s1, s2);
+}
+if (*s2 == '\0')
+return (1);
+if (*s1 != *s2)
 return (0);
+return (wildcmp(++s1, ++s2));
 }
